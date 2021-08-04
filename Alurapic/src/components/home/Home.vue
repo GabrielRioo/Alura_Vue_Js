7841dea134660bc857efa0623197581ee790935a
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-04 00:26:22 -0300

    adicionando componente de botão

--- Alurapic/src/components/home/Home.vue
+++ Alurapic/src/components/home/Home.vue
@@ -10,9 +10,20 @@
     />
 
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto">
+      <li
+        class="lista-fotos-item"
+        v-for="foto of fotosComFiltro"
+        v-bind:key="foto"
+      >
         <Meu-Painel :titulo="foto.titulo">
           <Imagem-Responsiva :url="foto.url" :titulo="foto.titulo" />
+          <Meu-Botao
+            tipo="button"
+            rotulo="REMOVER"
+            @botaoAtivado="remove(foto)"
+            :confirmacao="true"
+            estilo="perigo"
+          />
           <!-- <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" /> -->
         </Meu-Painel>
       </li>
@@ -22,43 +33,51 @@
 
 <script>
 import Painel from "../shared/painel/Painel.vue";
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
   components: {
     "Meu-Painel": Painel,
-    'Imagem-Responsiva': ImagemResponsiva,
+    "Imagem-Responsiva": ImagemResponsiva,
+    "Meu-Botao": Botao
   },
 
   data() {
     return {
       titulo: "Alurapic",
       fotos: [],
-      filtro: "",
+      filtro: ""
     };
   },
 
   created() {
     let promise = this.$http.get("http://localhost:3000/v1/fotos");
     promise
-      .then((res) => res.json())
+      .then(res => res.json())
       .then(
-        (fotos) => (this.fotos = fotos),
-        (err) => console.log(err)
+        fotos => (this.fotos = fotos),
+        err => console.log(err)
       );
   },
 
   computed: {
     fotosComFiltro() {
       // Se tiver algo preenchido
-      if(this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
         return this.fotos.filter(foto => exp.test(foto.titulo));
       } else {
         return this.fotos;
       }
     }
   },
+
+  methods: {
+    remove(foto) {
+      alert("Remover a foto " + foto.titulo);
+    }
+  }
 };
 </script>
 
@@ -74,8 +93,6 @@ export default {
 .lista-fotos-item {
   display: inline-block;
 }
-
-
 
 .filtro {
   display: block;
---
+++ Alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,48 @@
+<template>
+    <button :class="estiloDoBotao" :type="tipo" @click="disparaAcao()"> {{ rotulo }} </button>
+</template>
+
+<script>
+export default {
+    props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+
+    methods: {
+        disparaAcao() {
+            if(this.confirmacao) {
+                if(confirm('Deseja mesmo remover essa foto?')) {
+                    this.$emit('botaoAtivado');
+                }
+                return;
+            }
+            this.$emit('botaoAtivado')
+        }
+    },
+
+    computed: {
+        estiloDoBotao() {
+            if(this.estilo == 'padrao' || !this.estilo) return 'botao botao-padrao';
+            if (this.estilo == 'perigo') return 'botao botao-perigo';
+        }
+    }
+}
+</script>
+
+<style>
+    .botao {
+        display: inline-block;
+        padding: 10px;
+        border-radius: 3px;
+        margin: 10px;
+        font-size: 1.2em;
+    }
+
+    .botao-perigo {
+        background-color: firebrick;
+        color: white;
+    }
+
+    .botao-padrao {
+        background-color: darkcyan;
+        color: white;
+    }
+</style>
